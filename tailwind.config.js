b33dff858aeff9e16429d2176e8096a216ecf7e1
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-06 22:22:20 +0530

    added tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+        fontFamily: {
+            'helvetica': ['Helvetica', 'Arial', 'sans-serif'],
+            'okra': ['Okra', 'Helvetica', 'sans-serif']
+        },
+        spacing: {
+            '1/2': '50%'
+        }
+    },
+  },
+  plugins: [],
+}
+
