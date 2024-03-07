210e4ca33508dc9918d89f60e47b9893814d4a78
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-07 20:44:15 +0530

    home page layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
+import {HomeSectionComponent} from "./home-section/home-section.component";
+import {ContactComponent} from "./contact/contact.component";
+import {TrackingComponent} from "./tracking/tracking.component";
+import {StepsComponent} from "./steps/steps.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
     imports: [
-        HeaderComponent
+        HeaderComponent,
+        HomeSectionComponent,
+        ContactComponent,
+        TrackingComponent,
+        StepsComponent,
+        FooterComponent
     ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
