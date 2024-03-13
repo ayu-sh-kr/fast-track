a8dd9e6a56f89fdd41572091549e6779e13779c9
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-13 21:05:35 +0530

    main component for the admin section

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {AdminHeaderComponent} from "./admin-header/admin-header.component";
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'admin',
+  standalone: true,
+    imports: [
+        AdminHeaderComponent,
+        RouterOutlet
+    ],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css'
+})
+export class AdminComponent {
+
+}
