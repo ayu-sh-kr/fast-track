b0dc65e9c85789c9747303afee3e4e54c2262515
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-13 21:06:24 +0530

    modified header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,31 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, Renderer2, ViewChild} from '@angular/core';
+import {LowerCasePipe, NgForOf, NgStyle} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {ReplacePipe} from "../utils/replace.pipe";
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [],
+    imports: [
+        NgStyle,
+        NgForOf,
+        RouterLink,
+        LowerCasePipe,
+        ReplacePipe
+    ],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
     hide = false;
+
+
+    navItems = ['Home', 'Menu', 'Services', 'Contact Us'];
+
+    constructor(private renderer: Renderer2) {
+    }
+
 
     setHide() {
         this.hide = !this.hide;
