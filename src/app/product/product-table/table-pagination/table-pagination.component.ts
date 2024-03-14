76e2dc84372b08ff2844c845a3d70161b1bc23ce
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-14 21:38:24 +0530

    Change the theme and resolved pagination error

--- src/app/product/product-table/table-pagination/table-pagination.component.ts
+++ src/app/product/product-table/table-pagination/table-pagination.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {SelectOptionsComponent} from "../../../utils/element/select-options/select-options.component";
 import {CustomButtonComponent} from "../../../utils/element/custom-button/custom-button.component";
+import {count} from "rxjs";
 
 @Component({
   selector: 'table-pagination',
@@ -16,18 +17,51 @@ import {CustomButtonComponent} from "../../../utils/element/custom-button/custom
 export class TablePaginationComponent{
 
     @Input()
+    count!:number;
+
+    @Input()
     pageSize:number[] = [10, 20, 50];
 
     @Input()
-    size!:number;
+    size:number = 10;
 
     @Output()
     sizeChange = new EventEmitter<number>();
 
+    @Input()
+    start:number = 0;
+
+    @Output()
+    startChange = new EventEmitter<number>()
+
+    page: number = 0;
+
     handlePagination(event: Event) {
         let option = event.target as HTMLOptionElement
         let value = <number> <unknown> option.value
-        console.log(value);
         this.sizeChange.emit(value)
+    }
+
+    clickNext(){
+        if(! ((this.start + this.size) >= this.count)){
+            this.start += Number(this.size);
+            this.startChange.emit(this.start)
+            this.page = this.start / 10
+        }
+        console.log(`size: ${this.size} page: ${this.page}  start: ${this.start}`)
+    }
+
+    clickPrev(){
+        if(!(this.start == 0) && this.size < this.start){
+            this.start -= this.size;
+            this.startChange.emit(this.start);
+            this.page = this.start / 10
+        }
+        else{
+            this.start = 0;
+            this.startChange.emit(this.start);
+            this.page = this.start / 10
+        }
+        console.log(`size: ${this.size} page: ${this.page}  start: ${this.start}`)
     }
 }
