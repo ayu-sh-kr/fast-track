d0430cc5bf8e8c6f5753201fe6f02b5053fd917c
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-15 22:00:19 +0530

    Created a generalized nav component

--- src/app/utils/main-content/main-content.component.ts
+++ src/app/utils/main-content/main-content.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
-  selector: 'app-main-content',
+  selector: 'main-content',
   standalone: true,
-  imports: [],
+    imports: [
+        MatListItem,
+        MatNavList,
+        MatSidenav,
+        MatSidenavContainer,
+        MatSidenavContent,
+        RouterOutlet,
+        RouterLink
+    ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
 export class MainContentComponent {
+
+    @Input()
+    opened: boolean = false;
+
+    @Output()
+    openedChange = new EventEmitter<boolean>();
+
+    @Input()
+    sideBarItems!:NavItems[]
 
 }
