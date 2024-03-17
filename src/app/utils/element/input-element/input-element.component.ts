4289d206c36b98f15c0f5fba44df088dae62f236
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-17 10:15:37 +0530

    Custom input component that feature label and input tag

---
+++ src/app/utils/element/input-element/input-element.component.ts
@@ -0,0 +1,39 @@
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {MatDatepicker} from "@angular/material/datepicker";
+
+@Component({
+    selector: 'input-element',
+    standalone: true,
+    imports: [
+    ],
+    templateUrl: './input-element.component.html',
+    styleUrl: './input-element.component.css'
+})
+export class InputElementComponent {
+
+    @Input()
+    type:string = 'text';
+
+    @Input()
+    placeholder!: string;
+
+    @Input()
+    class!:string
+
+    @Input()
+    label!:string
+
+    @ViewChild('picker') datePicker!: MatDatepicker<Date>;
+
+    @Input()
+    data:any;
+
+    @Output()
+    dataChange = new EventEmitter<any>();
+
+    updateData(event: KeyboardEvent) {
+        let element = event.target as HTMLInputElement;
+        console.log(element.value)
+        this.dataChange.emit(element.value);
+    }
+}
