34b48b7463059832df6d0dea0dc10616b548b86f
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-18 21:45:19 +0530

    contains the type exported to get global access
    Enum can't be declared in .d.ts file

---
+++ src/app/utils/enums/exporter.ts
@@ -0,0 +1,9 @@
+export enum ShipmentStatus{
+    DELIVERED = 'Delivered',
+    PROCESSING = 'Processing',
+    DISPATCHED = 'Dispatched',
+    IN_TRANSITS = 'In-transits',
+    NEARBY = 'Nearby',
+    ARRIVED = 'Arrived',
+    DISPATCHING = 'Dispatching'
+}
