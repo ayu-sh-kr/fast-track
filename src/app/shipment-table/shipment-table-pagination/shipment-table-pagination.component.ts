1ffbf44521b9f905bd2994680ce43a73676b0a3b
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-18 21:47:50 +0530

    pagination for the shipment table

---
+++ src/app/shipment-table/shipment-table-pagination/shipment-table-pagination.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shipment-table-pagination',
+  standalone: true,
+  imports: [],
+  templateUrl: './shipment-table-pagination.component.html',
+  styleUrl: './shipment-table-pagination.component.css'
+})
+export class ShipmentTablePaginationComponent {
+
+}
