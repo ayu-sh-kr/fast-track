a046ee4a890d48b9e093a6162779aa251432142a
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-18 21:47:26 +0530

    header for the shipment table component

---
+++ src/app/shipment-table/shipment-table-header/shipment-table-header.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'shipment-table-header',
+    standalone: true,
+    imports: [],
+    templateUrl: './shipment-table-header.component.html',
+    styleUrl: './shipment-table-header.component.css'
+})
+export class ShipmentTableHeaderComponent {
+
+}
